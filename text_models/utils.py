bb2a7869f8753a5a8059e1b8904e79c117fe5c88
Mario Graff <mgraffg@gmail.com>
2020-03-11 11:19:53 -0600

    Utils

---
+++ text_models/utils.py
@@ -0,0 +1,51 @@
+# Copyright 2020 Mario Graff Guerrero
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from sklearn.metrics import f1_score, recall_score
+from scipy.stats import pearsonr
+
+
+def macro_f1(y, hy):
+    """
+    >>> from text_models.utils import macro_f1
+    >>> y = [0, 1, 1, 0]
+    >>> hy = [0, 1, 0, 0]
+    >>> macro_f1(y, hy)
+    0.7333333333333334
+    """
+
+    return f1_score(y, hy, average="macro")
+
+
+def macro_recall(y, hy):
+    """
+    >>> from text_models.utils import macro_recall
+    >>> y = [0, 1, 1, 0]
+    >>> hy = [0, 1, 0, 0]
+    >>> macro_recall(y, hy)
+    0.75
+    """
+
+    return recall_score(y, hy, average="macro")
+
+
+def pearson(y, hy):
+    """
+    >>> from text_models.utils import pearson
+    >>> y = [0, 1, 1, 0]
+    >>> hy = [0, 1, 0, 0]
+    >>> pearson(y, hy)
+    0.5773502691896258
+    """
+
+    return pearsonr(y, hy)[0]
