2aa46abd9991e68590590ffba5943e3531cf7fa6
Mario Graff <mgraffg@ieee.org>
2022-10-19 09:30:36 -0500

    Datasets Text Representations

--- text_models/inhouse/text_representations.py
+++ text_models/inhouse/text_representations.py
@@ -192,6 +192,12 @@ def dataset(lang, fname, name):
         return labels
     tm = load_bow(lang=lang)
     X = tm.transform(D)
+    if labels.shape[0] == 2:
+        m = LinearSVC().fit(X, [x['klass'] for x in D])
+        k = 1
+        output = join('models', f'{lang}_{name}_{k}_muTC{MICROTC}')    
+        save_model(m, f'{output}.LinearSVC')
+        return labels
     for k, label in enumerate(labels):
         output = join('models', f'{lang}_{name}_{k}_muTC{MICROTC}')    
         pos_index = np.array([i for i, x in enumerate(D) if x['klass'] == label])
